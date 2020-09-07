9f649ccfb6bba17f190ddb6a5a0c28e3217eb0da
IrynaOkun <iryna.ladybug@gmail.com>
2020-09-07 19:22:26 +0300

    retest8

--- specs/example/menu/menu_po.js
+++ specs/example/menu/menu_po.js
@@ -1,7 +1,7 @@
 class MenuPage {
     
     get menudropdown () {return $('a#navbarDropdown2.dropdown-toggle.nav-link')};
-    get settings () {return $('a.dropdown-item')};
+    get settings () {return $('body > app-root > app-work-space > nav > div > div.d-flex.order-lg-1.ml-auto.pr-2 > div.nav-item.show.dropdown > div > a:nth-child(1)')};
  
 
 };
