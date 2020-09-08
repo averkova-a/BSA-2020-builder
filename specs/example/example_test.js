11e742e93e885771cdc3d9d52eb88391b9f1810b
Andrii_Karun <andriy.karun@binary-studio.com>
2020-09-08 18:06:05 +0300

    added cdp service

--- specs/example/example_test.js
+++ specs/example/example_test.js
@@ -1,9 +1,16 @@
 describe('Your Playground test suite', () => {
     
     
-    it('Your Playground test', () =>{
-        browser.url('https://www.google.com/');
+    it('Your Playground test', () => {
+        browser.cdp('Network', 'enable')
+        browser.on('Network.responseReceived', (params) => {
+            console.log(`Loaded ${params.response.url}`)
+        })
+        browser.url('https://bsa-builder.xyz');
+        const puppeteer = browser.getPuppeteer()
+        const page = browser.call(() => puppeteer.pages())[0]
+        console.log("TITLE", browser.call(() => page.title()))
+        //find way to use async await
     });
 });
-
 
